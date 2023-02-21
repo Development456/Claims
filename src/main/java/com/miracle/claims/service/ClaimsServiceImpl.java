78b1679d9f57f3e97df4c7ec9d6c29736c48d16b
Development456 <120048356+Development456@users.noreply.github.com>
2023-02-21 13:44:54 -0500

    Update ClaimsServiceImpl.java

--- src/main/java/com/miracle/claims/service/ClaimsServiceImpl.java
+++ src/main/java/com/miracle/claims/service/ClaimsServiceImpl.java
@@ -12,7 +12,6 @@ import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
